28156cb1dcff4343ad883fd7751c671ffef3ec0d
Georgi Boshnakov <georgi.boshnakov@manchester.ac.uk>
2017-10-02 14:53:14 +0100

    bug fixes in documentation for version 0.22

---
+++ vignettes/Inserting_bibtex_references.R
@@ -0,0 +1,9 @@
+### R code from vignette source 'Inserting_bibtex_references.Rnw'
+
+###################################################
+### code chunk number 1: Inserting_bibtex_references.Rnw:163-165
+###################################################
+library(Rdpack)
+cat(insert_ref("R", package = "bibtex"), sep ="\n")
+
+
